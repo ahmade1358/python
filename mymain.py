d3b3a6ed8ddc7da1a5f0e737890251b552d597aa
ahmad ebrahimi <ahmad.e.nasb.1358@gmail.com>
2026-06-30 09:24:37 +0330

    aslahat dovom anjam shod

--- mymain.py
+++ mymain.py
@@ -1,23 +1,27 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
-from sklearn.preprocessing import StandardScaler,LabelEncoder
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from imblearn.over_sampling import SMOTE
 ###############################################################################
 # مشخص کردن بار گذاری فایل
 try:
-    df=pd.read_csv('hds.csv')
+    df=pd.read_csv("hds.csv")
     print("فایل با موفقیت بارگذاری شد")
 except FileNotFoundError:
     print("فایل پیدا نشد")
 ################################################################################
-copydf=df.copy() # یک کپی از اطلاعات می گیزیم
+copydf=df.copy() # یک کپی از اطلاعات می گیریم
 print(df.head())#نمایش پنج سطر اول
 print(df.info())#نمایش اطلاعی در بار ه تعداد فیلده ها و نوع انها
 ###############################################################################
 df=df.drop('id',axis=1)# حذف فیلد id
+
 print("فیلد id حذف شد")
 print(df.info())#نمایش دوباره اطلاعات فایل که مشخص می شود فیلد هid حذف شده
 #جای گزنی مقادیر ناشناس با مد
@@ -34,38 +38,74 @@ plt.tight_layout()
 plt.show()
 
 ###############################################################################
-#از روش KNN  استفاده کردیم 
-#جون  داده ها پژشکی هستند و فیلد ها باهم مرتب هستند یعنی سن ,جنسیت,مقدار قند خون و غیر  روی هم تاثیر دارند 
-#البته این روش اینجا خیلی کارمد نیست چون فیلد مورد نظر دادهای پرت نسبتا زیادی دارد
-print(df.isnull().sum())#مشخص کردن هر ستون چه مقدار داده خالی هست
-print(df['bmi'].describe())
-scol=['gender','ever_married','work_type','Residence_type','smoking_status']#فیلدهای متنی را مشخص می کنیم
+X=df.drop('stroke',axis=1)#هدف ما این هست که strok را پیش بینی کنیم بنابرین ان را حذف می کنیم 
+y = df['stroke']
+
+
+print("تعداد نمونه‌ها در هر کلاس:")
+print(df['stroke'].value_counts())
+# نمایش به صورت درصد (برای درک بهتر میزان ناترازی)
+print("\nدرصد هر کلاس در دیتاست:")
+print(df['stroke'].value_counts(normalize=True) * 100)
+
+plt.figure(figsize=(6, 4))
+sns.countplot(x='stroke', data=df, palette='viridis')
+plt.title('توزیع کلاس‌های هدف (Stroke vs No Stroke)')
+plt.show()
+
 ecod={}
-print('تبدیل ستون های متنی به عدد')
+scol=X.select_dtypes(include=['object']).columns.tolist()# مشخص کردن ستون های متنی
 for col in scol:
-    le=LabelEncoder()
-    df[col]=df[col].astype(str).fillna('Unknown')
-    df[col]=le.fit_transform(df[col])
-    ecod[col]=le
-    print(f"تبدیل '{col}'متنی")
-print("استانداردسازی داده ها")
-scal=StandardScaler()
-numcol=['bmi','age','hypertension','heart_disease','avg_glucose_level','stroke']#مشخص کردن فیلدهای عدی
-df[numcol]=scal.fit_transform(df[numcol])
-imputer=KNNImputer(n_neighbors=3)
-imputed_data=imputer.fit_transform(df)
-df_imputed=pd.DataFrame(imputed_data,columns=df.columns)
-df_imputed[numcol]=scal.inverse_transform(df_imputed[numcol])
-for col in scol:
-    df_imputed[col]=df_imputed[col].round().astype(int)
-    df_imputed[col]=df_imputed[col].clip(0,len(ecod[col].classes_)-1)
-    df_imputed[col]=ecod[col].inverse_transform(df_imputed[col])
-print(df_imputed.head(30))#چاپ کردن 30 سطر اول
+    statusv = X[col].nunique()
+    if statusv <= 2:
+        le = LabelEncoder()
+        X[col] = le.fit_transform(X[col])
+        ecod[col]=le
+    else:
+        newcol = pd.get_dummies(X[col], prefix=col,drop_first=True) # ساخت ستون‌های جدید (One-Hot)
+        X= pd.concat([X,newcol], axis=1) # چسباندن ستون‌های جدید
+        X.drop(col, axis=1, inplace=True) # حذف ستون متنی قدیمی
+
+print(f"تعداد NaN در bmi قبل: {X['bmi'].isna().sum()}")
+
+
+numcol =['age', 'avg_glucose_level', 'bmi']# scale  فقط روی ستون‌های عددی اصلی
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4, stratify=y)
+print(f"شکل X_train: {X_train.shape}")
+print(f"شکل X_test: {X_test.shape}")
+
+
+scaler = StandardScaler()#چون داده ها در یک بازه ثابت و مشخص نیستند 
+
+#X_train_scaled = X_train.copy()
+#X_test_scaled = X_test.copy()
+X_train[numcol] = scaler.fit_transform(X_train[numcol])
+X_test[numcol] = scaler.transform(X_test[numcol])
+
+#X_train_scaled[numcol] = scaler.fit_transform(X_train[numcol])
+#X_test_scaled[numcol] = scaler.transform(X_test[numcol])
+print("\nمقیاس داده‌های آموزش (بعد از StandardScaler):")
+print(X_train[numcol].describe().round(2))
+print("\nمقیاس داده‌های تست (بعد از StandardScaler):")
+print(X_test[numcol].describe().round(2))
+#  پر کردن مقادیر گمشده باKNNImputer
+imputer = KNNImputer(n_neighbors=5, weights='distance')
+X_train = pd.DataFrame(imputer.fit_transform(X_train), columns=X_train.columns)
+X_test = pd.DataFrame(imputer.transform(X_test), columns=X_test.columns)
+smote = SMOTE(random_state=42)
+X_train_r, y_train_r = smote.fit_resample(X_train, y_train)
+
+print(f"تعداد نمونه های  کلاس 1 قبل از SMOTE: {sum(y_train == 1)}")
+print(f"تعداد نمونه های کلاس 1 بعد از SMOTE: {sum(y_train_r == 1)}")
+
+print(f"تعداد NaN در bmi بعد: {X['bmi'].isna().sum()}")
+
+
 sns.set_theme(style="whitegrid")
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
 sns.histplot(copydf['bmi'], kde=True, ax=axes[0], color='gray')
 axes[0].set_title('قبل از جایگذازی')
-sns.histplot(df['bmi'], kde=True, ax=axes[1], color='green')
+sns.histplot(X['bmi'], kde=True, ax=axes[1], color='green')
 axes[1].set_title('بعد از جایگذاری به روش KNNI')
 plt.tight_layout()
 plt.show()
